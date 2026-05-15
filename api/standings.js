c91fef527abd047fe229e1f39abd515d7271506a
Cesar Vigil <cesarvigil2021@gmail.com>
2026-05-15 15:47:42 -0600

    Caching API from Sheets and updating standings.js to fetch from the new API endpoint.

--- api/standings.js
+++ api/standings.js
@@ -12,6 +12,11 @@ export default async function handler(req, res) {
 
     const data = await response.json()
 
+    res.setHeader(
+      "Cache-Control",
+      "public, s-maxage=3600, stale-while-revalidate=59"
+    )
+
     res.status(200).json(data)
 
   } catch (error) {
