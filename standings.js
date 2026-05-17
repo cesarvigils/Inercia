0bb142986bcb305a030282fcc4ae3afc46d775a7
cesarvigils <cesarvigil2021@gmail.com>
2026-05-16 23:39:38 -0600

    Tweak #4 PLEASE WORK

--- standings.js
+++ standings.js
@@ -133,6 +133,9 @@ async function loadStandings() {
 
       const esTeamInercia =
         nombre.includes("(T)")
+              const esTeamInercia =
+        nombre.includes("(S)")
+        
 
       // FILTROS
 
