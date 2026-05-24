7f74dcb2c6a89429c0ad4a1416c1cfc0d869f01d
Cesar Vigil <cesarvigil2021@gmail.com>
2026-05-23 19:03:43 -0600

    Remove redirect to "mis-reservas" after successful reservation creation in reservas.js

--- reservas.js
+++ reservas.js
@@ -640,7 +640,6 @@ if (reserveBtn) {
       }
 
       alert("Reserva creada correctamente.")
-      window.location.href = "/mis-reservas"
     } catch (error) {
       console.error(error)
       alert("Error al crear la reserva.")
