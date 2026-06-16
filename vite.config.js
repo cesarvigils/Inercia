6cc6228b4f48de267ad354939b52aa63e9ed3fb1
cesarvigils <cesarvigil2021@gmail.com>
2026-06-15 20:40:28 -0600

    a

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         index: resolve(__dirname, "index.html"),
-        standings: resolve(__dirname, "standings.html"),
-        contacto: resolve(__dirname, "contacto.html"),
+        /*standings: resolve(__dirname, "standings.html"), */
+                contacto: resolve(__dirname, "contacto.html"),
         perfil: resolve(__dirname, "perfil.html"),
         reservas: resolve(__dirname, "reservas.html"),
         admin: resolve(__dirname, "admin.html")
