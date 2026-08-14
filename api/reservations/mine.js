46b2ae77a7b208a27da25a1b48d581cdf78ceb6c
cesarvigils <cesarvigil2021@gmail.com>
2026-08-14 11:05:49 -0600

    Test

--- api/reservations/mine.js
+++
@@ -1,2 +0,0 @@
-import { method,json,fail,requireUser } from '../_lib/http.js'; import { adminDb } from '../_lib/firebase-admin.js';
-export default async function handler(req,res){try{if(!method(req,res,['GET']))return;const u=await requireUser(req);const s=await adminDb.collection('reservations').where('uid','==',u.uid).orderBy('createdAt','desc').limit(50).get();json(res,200,{reservations:s.docs.map(d=>({id:d.id,...d.data()}))});}catch(e){fail(res,e)}}
