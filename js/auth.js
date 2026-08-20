435095ec2425ed80ef05ee009f72c6508db404fd
cesarvigils <cesarvigil2021@gmail.com>
2026-08-19 20:26:04 -0600

    DF

--- js/auth.js
+++ js/auth.js
@@ -48,14 +48,6 @@ function injectAuthMarkup() {
       </label>
       <button class="auth-submit" type="submit">INICIAR SESIÓN</button>
       <div class="auth-divider"><span>O</span></div>
-        <svg viewBox="0 0 24 24" width="21" height="21" aria-hidden="true">
-          <path fill="#4285F4" d="M21.6 12.23c0-.71-.06-1.4-.18-2.07H12v3.92h5.38a4.6 4.6 0 0 1-2 3.02v2.54h3.24c1.9-1.75 2.98-4.33 2.98-7.41z"/>
-          <path fill="#34A853" d="M12 22c2.7 0 4.98-.9 6.64-2.43l-3.24-2.54c-.9.6-2.05.96-3.4.96-2.6 0-4.81-1.76-5.6-4.13H3.05v2.62A10 10 0 0 0 12 22z"/>
-          <path fill="#FBBC05" d="M6.4 13.86A6 6 0 0 1 6.08 12c0-.65.11-1.28.32-1.86V7.52H3.05A10 10 0 0 0 2 12c0 1.61.38 3.14 1.05 4.48l3.35-2.62z"/>
-          <path fill="#EA4335" d="M12 6.01c1.47 0 2.79.5 3.82 1.5l2.87-2.87A9.64 9.64 0 0 0 12 2a10 10 0 0 0-8.95 5.52l3.35 2.62C7.19 7.77 9.4 6.01 12 6.01z"/>
-        </svg>
-        CONTINUAR CON GOOGLE
-      </button>
       <p class="auth-switch">¿No tienes una cuenta? <button type="button" data-switch-auth="register">REGÍSTRATE</button></p>
     </form>
     <form class="auth-form" id="registerForm">
